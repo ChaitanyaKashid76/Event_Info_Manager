811f5514751a17ac10c8111e2701362bea8b8e65
Chaitanya Kashid <chaitanyakashid961@gmail.com>
2025-08-07 09:37:27 +0530

    Fix: removed extra Router to fix nested Router error

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-
-import './App.css'
 import Visitor from './Pages/VisitorPage'
 import Event from './Pages/EventPage'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import EventProvider from './Context/EventContext';
+import { Routes, Route } from 'react-router-dom'
+import EventProvider from './Context/EventContext'
+import './App.css'
 
 function App() {
-  
-
   return (
-    <>
     <EventProvider>
-      <Router>
       <Routes>
-        <Route path="/" element={<Visitor/>} />
+        <Route path="/" element={<Visitor />} />
         <Route path="/event" element={<Event />} />
       </Routes>
-    </Router>
     </EventProvider>
-    </>
   )
 }
 
